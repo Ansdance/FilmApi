b1ff66fbfb8c01befe9c3d53b9ee8b947a9c0283
ansardauletbayev <dauletbayev.ansar@gmail.com>
2023-01-13 21:24:16 +0600

    i fixed 2nd page of popular

--- FilmApi/PopularCollectionViewCell.swift
+++ FilmApi/PopularCollectionViewCell.swift
@@ -11,34 +11,12 @@ class PopularCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var popularImageCell: UIImageView!
     @IBOutlet weak var popularLabelCell: UILabel!
-    
-//    var popular: EntityPopular?
-//    
-//    override func awakeFromNib() {
-//        super.awakeFromNib()
-//        // Initialization code
-//    }
-//    
-//    func setCard(_ card:Card){
-//        // Keep track of the card that gets passed in
-//        frontImageView.image = UIImage(named: EntityPopular.self)
-//
-//    override func setSelected(_ selected: Bool, animated: Bool) {
-//        super.setSelected(selected, animated: animated)
-//
-//        // Configure the view for the selected state
-//    }
-//    func setData(film: EntityFilm) {
-//        popularLabelCell.text = film.title
-//        popularImageCell.sd_setImage(with: URL(string: film.poster_path), completed: nil)
-//        popularImageCell.layer.cornerRadius = 55
-//    }
-    
+        
     override func awakeFromNib() {
         super.awakeFromNib()
     }
 
-    func setData(film: EntityFilm) {
+    func setDataPop(film: EntityPopular) {
         popularLabelCell.text = film.title
         popularImageCell.sd_setImage(with: URL(string: film.poster_path), completed: nil)
         popularImageCell.layer.cornerRadius = 55
--- FilmApi/PopularViewController.swift
+++ FilmApi/PopularViewController.swift
@@ -15,6 +15,8 @@ class PopularViewController: UIViewController, UISearchBarDelegate, UICollection
     @IBOutlet var popularCollectionView: UICollectionView!
     
     private var popularArray : [EntityPopular] = []
+    var current_page = 1
+    var total_page = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,13 +25,13 @@ class PopularViewController: UIViewController, UISearchBarDelegate, UICollection
                 //Register cell
         self.popularCollectionView.register(UINib(nibName: "PopularViewController", bundle: nil), forCellWithReuseIdentifier: "PopularViewController")
                 //
-        searchFilms(query: 1)
+        searchFilms(page: 1)
             }
         
-    private func searchFilms(query: any Numeric){
-                                   let parameters = ["api_key": "Ы",
+    private func searchFilms(page: Int){
+                                   let parameters = ["api_key": "7de5f8b7cc960d1fb3bd9603ed5accf1",
                                                      "language" : "ru-RU",
-                                                     "page" : query ,
+                                                     "page" : page ,
                                                      "region": "ru"
                                                      ] as [String : Any]
                                    AF.request("https://api.themoviedb.org/3/movie/popular?", method: .get,
@@ -42,7 +44,6 @@ class PopularViewController: UIViewController, UISearchBarDelegate, UICollection
                     }
                     if responce.response?.statusCode == 200 {
                         let json = JSON(responce.data!)
-        
                         if let array = json["results"].array {
                             for item in array {
                                 let film = EntityPopular(json: item)
@@ -57,7 +58,7 @@ class PopularViewController: UIViewController, UISearchBarDelegate, UICollection
             func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
                 popularArray.removeAll()
                 popularCollectionView.reloadData()
-                searchFilms(query: 1)
+                searchFilms(page: 1)
             }
 }
 
@@ -74,106 +75,19 @@ extension PopularViewController: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! PopularCollectionViewCell
-//        let card = popularArray[indexPath.row]
-//        cell.popularLabelCell.text = popularArray[indexPath.row].title
-//        cell.popularImageCell.image = UIImage(named: popularArray[indexPath.row].poster_path)
-        cell.setData(film: popularArray[indexPath.row])
+        cell.setDataPop(film: popularArray[indexPath.row])
         return cell
     }
     func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-          print("Index Path \(indexPath)")
+        if current_page < total_page && indexPath.row == popularArray.count - 1 {
+            current_page = current_page + 1
+            searchFilms(page: current_page)
+        }
     }
-}
     
+        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            return 179
+        }
+}
 
-//    @IBOutlet private weak var popularSearchBar: UISearchBar!
-//
-//    @IBOutlet private weak var popularTableView: UITableView!
-//
-//    private var popularArray : [EntityPopular] = []
-//
-//    private let url = "https://api.themoviedb.org/3/search/movie?"
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        self.popularTableView.delegate = self
-//        self.popularTableView.dataSource = self
-//        self.popularTableView.rowHeight = UITableView.automaticDimension
-//        self.popularTableView.estimatedRowHeight = 44
-//
-//        //Register cell
-//        self.popularTableView.register(UINib(nibName: "PopularViewController", bundle: nil), forCellReuseIdentifier: "PopularViewController")
-//        self.popularSearchBar.delegate = self
-//        popularSearchBar.placeholder = "Search films"
-//        //
-//        searchFilms()
-//    }
-//
-//    private func searchFilms(){
-//                           let parameters = ["api_key": "7de5f8b7cc960d1fb3bd9603ed5accf1",
-//                                             "language" : "ru-RU",
-//                                             "page" : 1,
-//                                             "region": "ru"
-//                                             ] as [String : Any]
-//                           AF.request("https://api.themoviedb.org/3/movie/popular?", method: .get,
-//                   parameters: parameters).responseData {
-//            responce in
-//            var resultString = ""
-//            if let data = responce.data {
-//                resultString = String(data: data, encoding: .utf8)!
-//                print(resultString)
-//            }
-//            if responce.response?.statusCode == 200 {
-//                let json = JSON(responce.data!)
-//
-//                if let array = json["results"].array {
-//                    for item in array {
-//                        let film = EntityPopular(json: item)
-//                        self.popularArray.append(film)
-//                    }
-//                }
-//                self.popularTableView.reloadData()
-//            }
-//        }
-//    }
-//
-//    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-//        popularArray.removeAll()
-//        popularTableView.reloadData()
-//        searchFilms()
-//    }
-//}
-//
-//extension PopularViewController: UITableViewDelegate {
-//
-//}
-//
-//
-//extension PopularViewController: UITableViewDataSource {
-//    func numberOfSections(in tableView: UITableView) -> Int {
-//        return 1
-//    }
-//
-//
-//
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        // #warning Incomplete implementation, return the number of rows
-//        return self.popularArray.count
-//    }
-//
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        if let cell =  tableView.dequeueReusableCell(withIdentifier: "CellPopular", for: indexPath) as? PopularTableViewCell {
-//
-//            // Configure the cell...
-//            cell.setData(film: popularArray[indexPath.row])
-//
-//                return cell
-//    }
-//            return UITableViewCell()
-//}
-//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-//        return 179
-//    }
-//}
 
